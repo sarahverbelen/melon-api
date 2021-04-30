c5e03186cf087e1581bb7df0c83f6675f75fc85a
Sarah Verbelen <sarah.verbelen@studiohyperdrive.be>
2021-04-30 11:15:33 +0200

    feat: check name

--- dataFunctions.py
+++ dataFunctions.py
@@ -1,20 +1,25 @@
 import re
 
 import mongo
-from errors import NoEmailException, InvalidEmailException
+from errors import NoEmailException, InvalidEmailException, NoNameException
 
 emailRegex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
 
 def saveUser(object):
 	user = object.to_dict()
-	print(checkEmail(user))
+	checkName(checkEmail(user))
 
 def checkEmail(user):
 	if 'email' in user:
 		if re.search(emailRegex, user['email']):
-		 	# mongo.db.users.insert_one(user)
-			return 'email ok'
+			return user
 		else:
 		 	raise InvalidEmailException
 	else:
 		raise NoEmailException
+
+def checkName(user):
+	if 'name' in user:
+		mongo.db.users.insert_one(user)
+	else: 
+		raise NoNameException
--- errors.py
+++ errors.py
@@ -4,6 +4,10 @@ class NoEmailException(werkzeug.exceptions.HTTPException):
     code = 400
     description = 'Email is required'
 
+class NoNameException(werkzeug.exceptions.HTTPException):
+    code = 400
+    description = 'Name is required'
+
 class InvalidEmailException(werkzeug.exceptions.HTTPException):
     code = 400
     description = 'Email is invalid'
