87eb6d46ebfeac92c39458b4c08dc6f795e032ea
Sarah Verbelen <sarah.verbelen@studiohyperdrive.be>
2021-05-19 13:15:20 +0200

    fix: cors

--- app.py
+++ app.py
@@ -20,6 +20,47 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 bcrypt = Bcrypt(app)
 app.config["DEBUG"] = True
 
+def crossdomain(origin=None, methods=None, headers=None, # found here: https://stackoverflow.com/questions/26980713/solve-cross-origin-resource-sharing-with-flask
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 class JSONEncoder(json.JSONEncoder): # found here https://stackoverflow.com/questions/16586180/typeerror-objectid-is-not-json-serializable
     def default(self, o):
         if isinstance(o, ObjectId) or isinstance(o, Tag) or isinstance(o, datetime):
@@ -28,30 +69,36 @@ class JSONEncoder(json.JSONEncoder): # found here https://stackoverflow.com/ques
         return json.JSONEncoder.default(self, o)
 
 @app.route('/', methods=['GET'])
+@crossdomain(origin="*")
 def home():
     return "<h1>Melon API</h1>"
 
 @app.route('/test', methods=['GET'])
+@crossdomain(origin="*")
 def test():
 	return json.dumps(mongo.db.users.find_one(), cls=JSONEncoder)
 
 # REGISTER
 @app.route('/register', methods=['POST'])
+@crossdomain(origin="*")
 def save():
     return auth.register(flask.request.form, bcrypt)
 
 # LOGIN
 @app.route('/login', methods=['POST'])
+@crossdomain(origin="*")
 def login():
     return auth.login(flask.request.form, bcrypt)
 
 # GET USER BY ID
 @app.route('/user/<id>', methods=['GET'])
+@crossdomain(origin="*")
 def getUserById(id):
     return json.dumps(auth.getUserById(id),  cls=JSONEncoder)
 
 # SAVE RECORDS
 @app.route('/record', methods=['POST'])
+@crossdomain(origin="*")
 def saveRecord():
     auth_header = flask.request.headers.get('Authorization')
     # TODO: only when logged in
@@ -59,6 +106,7 @@ def saveRecord():
 
 # GET THE RECORDS OF A USER WITH FILTER (from querystring)
 @app.route('/record/', methods=['GET'])
+@crossdomain(origin="*")
 def getUserRecords():
     auth_header = flask.request.headers.get('Authorization')
     filter = {
@@ -79,18 +127,21 @@ def getUserRecords():
 
 # CHANGE THE SETTINGS
 @app.route('/settings', methods=['POST'])
+@crossdomain(origin="*")
 def changeSettings():
     auth_header = flask.request.headers.get('Authorization')
     return auth.editSettings(flask.request.form, auth_header)
 
 # GET THE CURRENT USER
 @app.route('/me', methods=['GET'])
+@crossdomain(origin="*")
 def getMe():
     auth_header = flask.request.headers.get('Authorization')
     return auth.getMe(auth_header)
 
 # DELETE THE CURRENT USER
 @app.route('/me/delete', methods=['GET'])
+@crossdomain(origin="*")
 def deleteMe():
     auth_header = flask.request.headers.get('Authorization')
     return auth.deleteUser(auth_header)
