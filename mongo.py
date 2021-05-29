63e2c06806faa2a120b74389b2c62870ceff031d
Sarah Verbelen <sarah.verbelen@studiohyperdrive.be>
2021-05-29 11:53:54 +0200

    fix: changed connection string

--- mongo.py
+++ mongo.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://admin:prototypeAdmin@melon-shard-00-00.igxsj.mongodb.net:27017,melon-shard-00-01.igxsj.mongodb.net:27017,melon-shard-00-02.igxsj.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-7ytrzl-shard-0&authSource=admin&retryWrites=true&w=majority")
+client = MongoClient("mongodb://admin:prototypeAdmin@melon-shard-00-00.igxsj.mongodb.net:27017,melon-shard-00-01.igxsj.mongodb.net:27017,melon-shard-00-02.igxsj.mongodb.net:27017/melon?ssl=true&ssl_cert_reqs=CERT_NONE&replicaSet=atlas-7ytrzl-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.melon
